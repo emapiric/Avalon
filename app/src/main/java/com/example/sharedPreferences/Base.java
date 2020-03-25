782840748dcc071a46dd86607c7112f14dc3aa25
Wobufet <markobabovic406@gmail.com>
2020-03-25 17:00:33 +0100

    Dodao sam sharedPreferences, ako ne bude jasno, javljajte

--- app/src/main/java/com/example/sharedPreferences/Base.java
+++ app/src/main/java/com/example/sharedPreferences/Base.java
@@ -12,12 +12,18 @@ public class Base {
     private final String preferencesKey = "PreferencesKey";
     private final String userKey = "userKey";
     private final String roomkey = "roomKey";
+    //U clasi ne moze da se inicijalizuje SharedPreferences da znate ! Mora u onCreate metodi
     public static SharedPreferences sharedPreferences;
 
+    //Sve ovo sto sam nazvao key , to je identifikator za posebne objekte u bazi (preciznije xml jer ovo SharedPref radi
+    //sa XML-om ) znaci prefernecesKey je bukvalno kljuc kojoj ti pristupas xml-u
+    //userKey je kljuc za pristup podatku gdje se nalazi njegova mac adresu u xml
+    //roomKey ista fora kao i userKey
     public String getPreferencesKey() {
         return preferencesKey;
     }
 
+    //Ovde ubacujemo mac adresu korisnika u bazu
     public void saveUserMacAddress() {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(userKey, getMacAddress());
@@ -25,21 +31,23 @@ public class Base {
 
     }
 
+    //Ovdje ubacujemo id sobe u bazu
     public void saveUserRoomId(String roomId) {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(roomkey, roomId);
         editor.commit();
 
     }
-
+//Vraca MAC adresu korisnika
     public String getUserMacAddress() {
         return sharedPreferences.getString(userKey, "User doesn't exist");
     }
-
+//Vraca id sobe u kojoj se korisnik nalazi
     public String getUserRoom() {
+        //Ovaj drugi parametar znaci ako nema nikakvog podatka sa roomKey , on vraca ovu poruju sto sam napisao ,,no room for this user''
         return sharedPreferences.getString(roomkey, "No room for this user");
     }
-
+//Metoda vraca MAC adresu korisnika
     public String getMacAddress() {
         try {
             List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
