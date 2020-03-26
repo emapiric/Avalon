f648b116fc6cc1fdeb39a0812d832dab9e581ea6
emapiric <57675835+emapiric@users.noreply.github.com>
2020-03-26 12:06:45 +0100

    PlayActivity dizajniran. Dohvaceni view-ovi

--- app/src/main/java/com/example/avalon/PlayActivity.java
+++ app/src/main/java/com/example/avalon/PlayActivity.java
@@ -3,20 +3,52 @@ package com.example.avalon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.avalon.domain.Player;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayActivity extends AppCompatActivity {
 
+    //Lista u koju ce se ucitati svi igraci kako bi se posle postavila imena u viewove
     List<Player> players;
+
+    //TextView u koji se upisuju dodatne informacije o igracima (npr za Merlina ko su zlikovci)
+    TextView tvInfo;
+    //ImageView u koji se smesta slika lika
+    ImageView ivCharacter;
+    //Lista TextViewova koji sadrze imena igraca i slicice pijuna
+    //prosto da nemamo 5 razlicitih TextView polja za sve nego ovako da prolazimo kroz njih
+    ArrayList<TextView> tvPlayersList = new ArrayList<>();
+    //NavigationBar za misije
+    BottomNavigationView navbarMission;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+        findViews();
+
+
+    }
+
+    private void findViews() {
+        tvInfo = findViewById(R.id.tv_info);
+        ivCharacter = findViewById(R.id.iv_character);
+        navbarMission = findViewById(R.id.navbar_mission);
+        tvPlayersList.add((TextView) findViewById(R.id.tv_player1));
+        tvPlayersList.add((TextView) findViewById(R.id.tv_player2));
+        tvPlayersList.add((TextView) findViewById(R.id.tv_player3));
+        tvPlayersList.add((TextView) findViewById(R.id.tv_player4));
+        tvPlayersList.add((TextView) findViewById(R.id.tv_player5));
     }
 }
